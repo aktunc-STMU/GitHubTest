7e14cde6d77d99fc27df38da22cd056ed86924e0
aktunc-STMU <aktunc@gmail.com>
2022-09-13 17:40:26 -0500

    Update MilesPerGallon.java

--- Java Basics and Challenges/MilesPerGallon.java
+++ Java Basics and Challenges/MilesPerGallon.java
@@ -7,7 +7,7 @@ input = JOptionPane.showInputDialog("Please enter the number of miles driven ");
 double milesDriven = Double.parseDouble(input);
 input = JOptionPane.showInputDialog("Please enter the number of gallons used ");
 double gallonsOfGasUsed = Double.parseDouble(input);
-Double MPG = milesDriven / gallonsOfGasUsed;
+Double MPG = milesDriven / gallonsOfGasUsed;  //use double not Double for basic variables
 JOptionPane.showMessageDialog(null, 
 "Caar's MPG: " + MPG);
 System.exit(0);
