4c810d3d0af871449c77844e8df26d3dc5163ff7
aktunc-STMU <aktunc@gmail.com>
2023-02-03 11:18:17 -0600

    Testing github

---
+++ EngStudent.java
@@ -0,0 +1,29 @@
+
+public class EngStudent extends Student {
+	
+	Grade grade;
+
+	public EngStudent(int id, String name, Grade grade) {
+		super(id, name);
+		
+	}
+
+	
+	public Grade getGrade() {
+		return grade;
+	}
+
+
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+
+
+	@Override
+	public String toString() {
+		return "EngStudent [grade=" + grade.calculateGrade() + "]";
+	}
+	
+	
+
+}
